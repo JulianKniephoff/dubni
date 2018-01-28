0e362ae3927cffa383b4a73a6c35dc250b567bc3
Julian Kniephoff <me@juliankniephoff.com>
2018-01-28 14:14:18 +0100

    Randomly colored "full canvas" rect

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
+#![recursion_limit = "1024"]
+
 #[macro_use]
 extern crate stdweb;
+
+use stdweb::web;
 
 fn main() {
     stdweb::initialize();
 
+    let ref canvas = web::document().get_element_by_id("screen").unwrap();
+    let ref ctx = js! { return @{canvas}.getContext("2d"); };
     js! {
-        console.log("Hello, World!");
-    }
+        @{ctx}.fillStyle = "rgb("
+            + Math.floor(256 * Math.random()) + ", "
+            + Math.floor(256 * Math.random()) + ", "
+            + Math.floor(256 * Math.random()) + ")";
+        @{ctx}.fillRect(0, 0, @{canvas}.width, @{canvas}.height);
+    };
 
     stdweb::event_loop();
 }
